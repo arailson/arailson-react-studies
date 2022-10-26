fa845c8e90cd0e8dc0f8e221cfea60b74bd3933b
Arailson <arailson.vieira@gmail.com>
2022-10-26 15:21:03 -0300

    Finalizado

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -15,18 +15,5 @@ function Botao({ onClick, type, texto }: Props) {
   );
 }
 
-// class Botao1 extends React.Component<{
-//   texto: string;
-//   type?: "button" | "submit" | "reset";
-//   onClick?: () => void;
-// }> {
-//   render() {
-//     const { type = "button", onClick } = this.props;
-//     return (
-//       <button onClick={onClick} type={type} className={style.botao}>
-//         {this.props.texto}
-//       </button>
-//     );
-//   }
-// }
+
 export default Botao;
