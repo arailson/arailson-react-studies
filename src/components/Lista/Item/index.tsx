249435694472497950a39d0d8438e1f877715b26
Arailson <arailson.vieira@gmail.com>
2022-10-26 14:57:26 -0300

    Finalizando tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -13,11 +13,13 @@ export default function Item({
   id,
   selecionaTarefa,
 }: Props) {
-  
   return (
     <li
-      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ""} ${
+        completado ? style.itemCompletado : ""
+      }`}
       onClick={() =>
+        !completado &&
         selecionaTarefa({
           tarefa,
           tempo,
@@ -29,6 +31,9 @@ export default function Item({
     >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {completado && (
+        <span className={style.concluido} aria-label="Tarefa Completada"></span>
+      )}
     </li>
   );
 }
