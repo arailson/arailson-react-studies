72ba0792ab958bf2370616fc96208d69ad64cd89
Arailson <arailson.vieira@gmail.com>
2022-10-26 14:37:34 -0300

    Regressiva

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -4,11 +4,12 @@ import style from "./Botao.module.scss";
 class Botao extends React.Component<{
   texto: string;
   type?: "button" | "submit" | "reset";
+  onClick?: () => void;
 }> {
   render() {
-    const { type = "button" } = this.props;
+    const { type = "button", onClick } = this.props;
     return (
-      <button type={type} className={style.botao}>
+      <button onClick={onClick} type={type} className={style.botao}>
         {this.props.texto}
       </button>
     );
